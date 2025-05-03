0a016a7130b327fc6392c1b50e86364ef5c09c06
Sashi Bhushan A Rajput <sam556913@gmail.com>
2025-05-03 23:15:46 +0530

    fixed import issue

---
+++ src/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/bishopmate/kafka-implementation-go/src/models/responsemodels"
+)
+
+var _ = net.Listen
+var _ = os.Exit
+
+func main() {
+
+	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	defer l.Close()
+	// fmt.Printf("Type: %T\n", l)
+	if err != nil {
+		fmt.Println("Failed to bind to port 9092")
+		os.Exit(1)
+	}
+
+	conn, err := l.Accept()
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+	fmt.Println(string(buffer))
+
+	messageSize := int32(1)
+	correlationId := int32(7)
+
+	mr := responsemodels.NewMessageResponse(messageSize, correlationId)
+	conn.Write(mr.GetBytes())
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+}
